e5c702227f8391eca7eabf38f94e3d46292fbc79
Matheus Toscano <ssstoscano@gmail.com>
2018-06-27 22:51:58 -0300

    II
    Defining the detection function

--- Module_1_Face_Recognition/face_recognition_nocomment.py
+++ Module_1_Face_Recognition/face_recognition_nocomment.py
@@ -1,8 +1,14 @@
-# Face Recognition
+#Face Recognition
 
-# Importing the libraries
+#Importing the libraries
 import cv2
 
-# Loading the cascades
+#Loading the cascades
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#Defining a fucntion to detect
+def detect(gray, frame):
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
