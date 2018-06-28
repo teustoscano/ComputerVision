97b5f189f25e7c182d680ee5fb8be4d236820825
Matheus Toscano <ssstoscano@gmail.com>
2018-06-27 23:01:52 -0300

    III
    Finishing detection function

--- Module_1_Face_Recognition/face_recognition_nocomment.py
+++ Module_1_Face_Recognition/face_recognition_nocomment.py
@@ -12,3 +12,14 @@ def detect(gray, frame):
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for(x,y,w,h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
+        for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0,255,255), 2)
+    return frame
+
+#Face recognition with webcam
+
+
+        
